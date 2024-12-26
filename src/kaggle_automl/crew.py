be98c35bf1b45a4d4597e08cf8e83f0e2cfeac04
Anant Chandra <anantcha@usc.edu>
2024-12-26 09:12:17 +0530

    Add AutoML agents & delete boilerplate code

--- src/kaggle_automl/crew.py
+++ src/kaggle_automl/crew.py
@@ -4,18 +4,11 @@ from crewai import Agent, Crew, LLM, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from .tools import kaggle_download_tool, kaggle_metadata_extractor_tool
 
-# If you want to run a snippet of code before or after the crew starts, 
-# you can use the @before_kickoff and @after_kickoff decorators
-# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
-
 
 @CrewBase
 class KaggleAutoml:
 	"""KaggleAutoml crew"""
 
-	# Learn more about YAML configuration files here:
-	# Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
-	# Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
 	agents_config = 'config/agents.yaml'
 	tasks_config = 'config/tasks.yaml'
 
@@ -26,11 +19,10 @@ class KaggleAutoml:
 		api_base="https://api.openai.com/v1"
 	)
 
-	# If you would like to add tools to your agents, you can learn more about it here:
-	# https://docs.crewai.com/concepts/agents#agent-tools
 	@agent
 	def dataset_assessor(self) -> Agent:
 		return Agent(
+			allow_delegation=False,
 			config=self.agents_config['dataset_acquisition_specialist'],
 			llm=self.openai_llm,
 			tools=[
@@ -40,54 +32,17 @@ class KaggleAutoml:
 			verbose=True
 		)
 
-	@agent
-	def researcher(self) -> Agent:
-		return Agent(
-			config=self.agents_config['researcher'],
-			llm=self.openai_llm,
-			verbose=True
-		)
-
-	@agent
-	def reporting_analyst(self) -> Agent:
-		return Agent(
-			config=self.agents_config['reporting_analyst'],
-			llm=self.openai_llm,
-			verbose=True
-		)
-
-	# To learn more about structured task outputs, 
-	# task dependencies, and task callbacks, check out the documentation:
-	# https://docs.crewai.com/concepts/tasks#overview-of-a-task
 	@task
 	def dataset_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['dataset_acquisition_task'],
-		)
-
-	@task
-	def research_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['research_task'],
-		)
-
-	@task
-	def reporting_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['reporting_task'],
-			output_file='report.md'
-		)
+		return Task(config=self.tasks_config['dataset_acquisition_task'])
 
 	@crew
 	def crew(self) -> Crew:
 		"""Creates the KaggleAutoml crew"""
-		# To learn how to add knowledge sources to your crew, check out the documentation:
-		# https://docs.crewai.com/concepts/knowledge#what-is-knowledge
 
 		return Crew(
-			agents=self.agents, # Automatically created by the @agent decorator
-			tasks=self.tasks, # Automatically created by the @task decorator
+			agents=self.agents,
+			tasks=self.tasks,
 			process=Process.sequential,
 			verbose=True,
-			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
 		)
