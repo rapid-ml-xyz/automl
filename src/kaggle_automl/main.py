4bcad832f5f19c20f7ff5a3f8766490501a019fe
Anant Chandra <anantcha@usc.edu>
2025-01-05 20:46:48 +0530

    Add iteration via cli

--- src/kaggle_automl/main.py
+++ src/kaggle_automl/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import warnings
 import argparse
+from kaggle_automl.few_shot_crew import FewShot
 from kaggle_automl.zero_shot_crew import ZeroShot
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
@@ -12,23 +13,35 @@ def parse_args():
     """
     parser = argparse.ArgumentParser(description='KaggleAutoML CLI')
     parser.add_argument('command', choices=['run'], help='Command to execute')
-    parser.add_argument('--topic', type=str, required=False, help='Topic for analysis')
+    parser.add_argument('--action', type=str, required=True, help='Action to be performed')
+    parser.add_argument('--max-iterations', type=int, default=5, required=True, help='Maximum number of iterations')
     return parser.parse_args()
 
 
-def run(topic):
+# TODO: Add smart stop condition
+def run(action, max_iterations=5):
     """
     Run the crew.
     """
-    inputs = {'topic': topic}
+    inputs = {'topic': action}
     ZeroShot().crew().kickoff(inputs=inputs)
+
+    iteration_count = 0
+    few_shot_crew = FewShot().crew()
+
+    while iteration_count < max_iterations:
+        few_shot_crew.kickoff()
+        iteration_count += 1
+
+    if iteration_count == max_iterations:
+        print("Maximum iterations reached. Exiting.")
 
 
 def main():
     args = parse_args()
 
     if args.command == 'run':
-        run(args.topic)
+        run(args.action, args.max_iterations)
     else:
         raise Exception("Invalid command")
 
